5dd25d7390eea398a575fb3a8a9e8b9bed57906e
muhammad <111753966+muhammad-12345@users.noreply.github.com>
2024-07-15 15:10:51 +0500

    converted normal functions into arrow functions

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-let leadingSeconds = 0;
-let leadingMinutes = 0;
-let leadingHours = 0;
-
 let timerInterval = null;
 
-function stopWatch(){
+const checkNumberLessThanTen = (number) => {
+    return number<10?`0${number}`:number;
+};
+
+
+const displayTimer = (hours, minutes, seconds) => {
+    document.getElementById("timer").innerText = `${checkNumberLessThanTen(hours)}:${checkNumberLessThanTen(minutes)}:${checkNumberLessThanTen(seconds)}`;
+};
+
+
+// function stopWatch(){
+//     seconds++;
+//     if(seconds/60===1){
+//         seconds=0;
+//         minutes++;
+//         if(minutes/60===1){
+//             minutes=0;
+//             hours++;
+//         }
+//     }
+//     displayTimer(hours,minutes,seconds);
+// }
+
+const stopWatch = () => {
     seconds++;
     if(seconds/60===1){
         seconds=0;
@@ -22,44 +41,26 @@ function stopWatch(){
             hours++;
         }
     }
-
-    if(seconds<10){
-        leadingSeconds = "0" + seconds.toString();
-    }else{
-        leadingSeconds = seconds;
-    }
-
-    if(minutes<10){
-        leadingMinutes = "0" + minutes.toString();
-    }else{
-        leadingMinutes = minutes;
-    }
-
-    if(hours<10){
-        leadingHours = "0" + hours.toString();
-    }else{
-        leadingHours = hours;
-    }
-
-    let displayTimer = document.getElementById("timer").innerText = leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
-}
+    displayTimer(hours,minutes,seconds);
+};
 
 
-startBtn.addEventListener('click',function(){
-    timerInterval = window.setInterval(stopWatch,1000);
+// startBtn.addEventListener('click',function(){
+//     timerInterval = window.setInterval(stopWatch,1000);
+// });
+startBtn.addEventListener('click', () => {
+    timerInterval = window.setInterval(stopWatch, 1000);
 });
 
-pauseBtn.addEventListener('click',function(){
+pauseBtn.addEventListener('click', () => {
     window.clearInterval(timerInterval);
 });
 
-stopBtn.addEventListener('click',function(){
+stopBtn.addEventListener('click', () => {
     window.clearInterval(timerInterval);
-    seconds=0;
-    minutes=0;
-    hours=0;
-    console.log("khsdb");
-
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
     document.getElementById('timer').innerHTML = "00:00:00";
 });
 
